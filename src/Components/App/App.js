35a98bc7eab7a993a34e11a83e168215b6f473c8
gonzs <gonzalosisnero@gmail.com>
2021-01-23 11:37:44 -0300

    Refactor : Add HasRouter

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { Store } from '../../Redux/Store';
 import { NavBar } from '../NavBar';
 import { Header } from '../Header';
@@ -9,13 +9,13 @@ export const App = function App() {
   return (
     <div>
       <Store>
-        <BrowserRouter basename="/quiz">
+        <HashRouter basename="/">
           <Header />
 
           <NavBar />
 
           <Router />
-        </BrowserRouter>
+        </HashRouter>
       </Store>
     </div>
   );
