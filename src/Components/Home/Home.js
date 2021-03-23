a87b80a9105a3c71a40a04c2812f32c88ea9749c
gonzs <gonzalosisnero@gmail.com>
2021-03-23 20:16:24 -0300

    Feature : Adding more info to home

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -6,13 +6,41 @@ import { Jumbotron, Button } from 'react-bootstrap';
  */
 
 export const Home = () => {
+  const [learnMore, setLearnMore] = React.useState(false);
+
+  const content = learnMore ? (
+    <>
+      <p>
+        To enjoy the power of Quiz App you have to be logged.
+        <br />
+        Once you are login in, will see different trivias ready to be solved.
+        <br />
+        Good Luck!!!
+        <br />
+        <br />
+        <br />
+        ***DISCLAIMER***
+        <ul> - Admin users are only able to create new trivias.</ul>
+        <ul> - All user can display all trivias.</ul>
+        <br />
+        <b>We are working to add new features.</b>
+      </p>
+    </>
+  ) : (
+    <p>
+      <Button variant="secondary" onClick={() => setLearnMore(!learnMore)}>
+        Learn more
+      </Button>
+    </p>
+  );
+
   return (
     <Jumbotron>
-      <h1>Hello, Welcome to this QUIZ!</h1>
-      <p>This is a simple quiz</p>
-      <p>
-        <Button variant="secondary">Learn more</Button>
-      </p>
+      <h1>Hello, Welcome to this Quiz App!</h1>
+      <br />
+      <h4>This is just a quiz</h4>
+      <br />
+      {content}
     </Jumbotron>
   );
 };
