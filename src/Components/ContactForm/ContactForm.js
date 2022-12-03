295c9656b24a14ce2a1c3ce94e6d16819d6955a7
gonzs <gonzalosisnero@gmail.com>
2022-12-03 00:45:56 -0300

    add `ContactForm`

---
+++ src/Components/ContactForm/ContactForm.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Button, Col, Form, Row } from 'react-bootstrap';
+
+export default function ContactForm() {
+  return (
+    <Form name="contact" method="POST" data-netlify="true">
+      <Row>
+        <Col xs={3}>
+          <Form.Group controlId="formBasicEmail">
+            <Form.Control type="email" placeholder="your@email.com" />
+          </Form.Group>
+        </Col>
+        <Col>
+          <Button variant="primary" type="submit">
+            Send
+          </Button>
+        </Col>
+      </Row>
+    </Form>
+  );
+}
