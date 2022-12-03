7d3b39157539a710cbd87267eb0939283c316162
gonzs <gonzalosisnero@gmail.com>
2022-12-03 01:18:43 -0300

    fix CI check

--- src/Components/ContactForm/ContactForm.js
+++ src/Components/ContactForm/ContactForm.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, Col, Form, Row } from 'react-bootstrap';
+// import { Button, Col, Form, Row } from 'react-bootstrap';
 
 export default function ContactForm() {
   return (
