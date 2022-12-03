cb601aae769d1442db4eef2acb7cab9a33078aa8
gonzs <gonzalosisnero@gmail.com>
2022-12-03 20:52:54 -0300

    replace HashRouter with BrowserRouter

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { HashRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import { Store } from '../../Redux/Store';
 import { NavBar } from '../NavBar';
 import { Header } from '../Header';
@@ -13,13 +13,13 @@ export const App = function App() {
   return (
     <div>
       <Store>
-        <HashRouter basename="/">
+        <BrowserRouter basename="/">
           <Header />
 
           <NavBar />
 
           <Router />
-        </HashRouter>
+        </BrowserRouter>
       </Store>
     </div>
   );
