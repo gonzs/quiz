abf6ec1e96ad05544e0065a84a8e2402a6cd3b4f
gonzs <gonzalosisnero@gmail.com>
2022-12-03 21:16:59 -0300

    add attr `name`

--- src/Components/ContactForm/ContactForm.js
+++ src/Components/ContactForm/ContactForm.js
@@ -8,7 +8,11 @@ export default function ContactForm() {
       <Row>
         <Col xs={3}>
           <Form.Group controlId="formBasicEmail">
-            <Form.Control type="email" placeholder="your@email.com" />
+            <Form.Control
+              name="email"
+              type="email"
+              placeholder="your@email.com"
+            />
           </Form.Group>
         </Col>
         <Col>
@@ -18,5 +22,26 @@ export default function ContactForm() {
         </Col>
       </Row>
     </form>
+    // <form name="contact" method="post">
+    //   <input type="hidden" name="form-name" value="contact" />
+    //   <p>
+    //     <label>
+    //       Your Name: <input type="text" name="name" />
+    //     </label>
+    //   </p>
+    //   <p>
+    //     <label>
+    //       Your Email: <input type="email" name="email" />
+    //     </label>
+    //   </p>
+    //   <p>
+    //     <label>
+    //       Message: <textarea name="message"></textarea>
+    //     </label>
+    //   </p>
+    //   <p>
+    //     <button type="submit">Send</button>
+    //   </p>
+    // </form>
   );
 }
