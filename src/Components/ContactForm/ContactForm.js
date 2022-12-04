2922848febcd0e94f20ac075bc1c7b43a891792b
gonzs <gonzalosisnero@gmail.com>
2022-12-03 21:17:10 -0300

    .

--- src/Components/ContactForm/ContactForm.js
+++ src/Components/ContactForm/ContactForm.js
@@ -22,26 +22,5 @@ export default function ContactForm() {
         </Col>
       </Row>
     </form>
-    // <form name="contact" method="post">
-    //   <input type="hidden" name="form-name" value="contact" />
-    //   <p>
-    //     <label>
-    //       Your Name: <input type="text" name="name" />
-    //     </label>
-    //   </p>
-    //   <p>
-    //     <label>
-    //       Your Email: <input type="email" name="email" />
-    //     </label>
-    //   </p>
-    //   <p>
-    //     <label>
-    //       Message: <textarea name="message"></textarea>
-    //     </label>
-    //   </p>
-    //   <p>
-    //     <button type="submit">Send</button>
-    //   </p>
-    // </form>
   );
 }
